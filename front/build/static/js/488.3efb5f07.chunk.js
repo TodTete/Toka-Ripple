a412d2075c720065d54fcede50d8e95318ca27f9
TodTete <vallejoricardo3@gmail.com>
2026-04-08 04:15:36 -0600

    conection with Toka 7

--- front/build/static/js/488.3efb5f07.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(globalThis.webpackChunkfront=globalThis.webpackChunkfront||[]).push([[488],{488(e,a,n){new WeakMap;new Set}}]);
-//# sourceMappingURL=488.3efb5f07.chunk.js.map
--- front/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the Toka Ripple shell', () => {
-  render(<App />);
-  expect(screen.getByText(/Mini App web funcional/i)).toBeInTheDocument();
-});
